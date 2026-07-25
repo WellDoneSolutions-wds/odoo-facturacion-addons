efb14a51b67ab74a02fd5d508478f2caafd36f2a
Helard Joel Gomez Apaza <hgomezap@emeal.nttdata.com>
2026-07-25 00:54:19 -0500

    test(password): el reset por email revoca las API keys (pin de comportamiento)

--- addons/l10n_pe_ne_biller/tests/test_password_reset.py
+++ addons/l10n_pe_ne_biller/tests/test_password_reset.py
@@ -70,6 +70,15 @@ class TestPasswordReset(TransactionCase):
         self.env['res.users'].with_user(self.user_a).l10n_pe_ne_change_own_password('oldpass12', 'nuevapass56')
         self.assertFalse(self.env['res.users.apikeys'].sudo().search([('user_id', '=', self.user_a.id)]))
 
+    def test_confirm_reset_revokes_apikeys(self):
+        self.env['res.users.apikeys'].with_user(self.user_a).sudo()._generate('l10n_pe_ne', 'test', False)
+        partner = self.user_a.partner_id
+        partner.signup_prepare(signup_type='reset')
+        token = partner._generate_signup_token()
+        res = self.env['res.users'].l10n_pe_ne_confirm_password_reset(token, 'clavenueva99')
+        self.assertEqual(res, {'ok': True})
+        self.assertFalse(self.env['res.users.apikeys'].sudo().search([('user_id', '=', self.user_a.id)]))
+
     def test_list_users_non_admin_raises(self):
         with self.assertRaises(AccessError):
             self.env['res.users'].with_user(self.user_a).l10n_pe_ne_list_manageable_users()
